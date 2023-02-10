f6d4dd95ac1141bad33b94c3d70ac088050f1a1d
Natasha Draper <natasha.draper@entelect.co.za>
2023-02-10 13:57:51 +0200

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { onMount } from 'svelte';
+  import { themeChange } from 'theme-change';
+  import '../app.scss';
+
+  onMount(() => {
+    // Register theme change
+    themeChange(false);
+  });
+</script>
+
+<div class="flex flex-col justify-center items-center">
+  <slot />
+</div>
